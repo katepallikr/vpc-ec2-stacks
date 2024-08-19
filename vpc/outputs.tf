e9d6d0ef64c7c4af6b3fc8363d16eb21904fe46b
Sarah Hernandez <sarah.hernandez@hashicorp.com>
2024-08-19 08:54:46 -0700

    adding license headers

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 output "vpc_id" {
   description = "The ID of the VPC created."
 
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 variable "availability_zones" {
   description = "List of availability zones in the region"
   type        = list(string)
